b94d55945f2d1ba332b3f45fdee27fcb12d964a2
knoxhack <hackofgod@gmail.com>
2020-07-06 19:02:57 -0400

    Added more ores

--- src/main/java/com/knoxhack/moreores/setup/RegistrationHandler.java
+++ src/main/java/com/knoxhack/moreores/setup/RegistrationHandler.java
@@ -25,7 +25,14 @@ public class RegistrationHandler {
 	public static final RegistryObject<EnderOre> ENDERORE = BLOCKS.register("ender_ore", EnderOre::new);
     public static final RegistryObject<Item> ENDERORE_ITEM = ITEMS.register("ender_ore", () -> new BlockItem(ENDERORE.get(), new Item.Properties().group(MoreOres.TAB)));
     
+	public static final RegistryObject<EnderOre> FLINT_ORE = BLOCKS.register("flint_ore", EnderOre::new);
+    public static final RegistryObject<Item> FLINT_ORE_ITEM = ITEMS.register("flint_ore", () -> new BlockItem(FLINT_ORE.get(), new Item.Properties().group(MoreOres.TAB)));
     
+	public static final RegistryObject<EnderOre> GLOWSTONE_ORE = BLOCKS.register("glowstone_ore", EnderOre::new);
+    public static final RegistryObject<Item> GLOWSTONE_ORE_ITEM = ITEMS.register("glowstone_ore", () -> new BlockItem(GLOWSTONE_ORE.get(), new Item.Properties().group(MoreOres.TAB)));
+    
+	public static final RegistryObject<EnderOre> GUNPOWDER_ORE = BLOCKS.register("gunpowder_ore", EnderOre::new);
+    public static final RegistryObject<Item> GUNPOWDER_ORE_ITEM = ITEMS.register("gunpowder_ore", () -> new BlockItem(GUNPOWDER_ORE.get(), new Item.Properties().group(MoreOres.TAB)));
     
     
     
